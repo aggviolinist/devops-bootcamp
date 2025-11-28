4e1df3b47052ead8433c0355f9c7035883830ab2
aggviolinist <mulandimaei76@gmail.com>
2025-11-28 13:35:32 +0300

    iam update

--- Project-3-ecs-with-terraform/terraform-aws-ecs-architecture/modules/management/main.tf
+++ Project-3-ecs-with-terraform/terraform-aws-ecs-architecture/modules/management/main.tf
@@ -1,7 +1,7 @@
 #Policy to give EC2 tasks access to s3
 resource "aws_iam_policy" "custom_policy_for_s3" {
-  name        = "ec2-policy-s3"
-  description = "Policy to give EC2 tasks access s3"
+  name        = "policy-s3"
+  description = "Policy to give EC2 and ECS role access s3"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -10,6 +10,7 @@ resource "aws_iam_policy" "custom_policy_for_s3" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
+          "s3:CreateBucket",
           "s3:ListBucket",
           "s3:ListAllMyBuckets"
         ]
@@ -19,10 +20,10 @@ resource "aws_iam_policy" "custom_policy_for_s3" {
   })
 }
 
-#Policy to give EC2 and ECS access to secrets manager
+#Policy to give EC2 and ECS task manager access to secrets manager
 resource "aws_iam_policy" "custom_policy_for_secrets_manager" {
-  name        = "ec2-and-ecs-policy-secrets-manager"
-  description = "Policy to give EC2 and ECS tasks access to secrets manager"
+  name        = "policy-for-secrets-manager"
+  description = "Policy to give EC2, ECS tasks and ECS task executor access to secrets manager"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -38,10 +39,11 @@ resource "aws_iam_policy" "custom_policy_for_secrets_manager" {
     ]
   })
 }
+
 #Role to give EC2 tasks access to s3 and secrets manager
-resource "aws_iam_role" "custom_role_for_ec2_s3_secrets_manager" {
-  name        = "ec2-role-for-s3-secrets-manager"
-  description = "Role to give ECS tasks access to secrets manager and s3"
+resource "aws_iam_role" "custom_role_for_ec2_policies" {
+  name        = "ec2-role-policies"
+  description = "Role to give EC2 access to secrets manager and s3"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -58,9 +60,9 @@ resource "aws_iam_role" "custom_role_for_ec2_s3_secrets_manager" {
 }
 
 #Role for ECS TASK ROLE to secrets manager
-resource "aws_iam_role" "custom_role_for_ecs_task_secrets_manager" {
-  name        = "ecs-task-role-for-secrets-manager"
-  description = "Role to give ECS tasks role access to secrets manager"
+resource "aws_iam_role" "custom_role_for_ecs_task_policies" {
+  name        = "ecs-task-role"
+  description = "Role to give ECS tasks role access to secrets manager,s3,ssm"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -77,9 +79,9 @@ resource "aws_iam_role" "custom_role_for_ecs_task_secrets_manager" {
 }
 
 #Role for ECS TASK AGENT to secrets manager
-resource "aws_iam_role" "custom_role_for_ecs_task_execution_secrets_manager" {
-  name        = "ecs-task-execution-role-for-secrets-manager"
-  description = "Role to give ECS agent access to secrets manager"
+resource "aws_iam_role" "custom_role_for_ecs_task_execution_policies" {
+  name        = "ecs-task-execution-role"
+  description = "Role to give ECS agent access to ssm,secrets manager,cloudwatch logs and ecr"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -95,29 +97,49 @@ resource "aws_iam_role" "custom_role_for_ecs_task_execution_secrets_manager" {
   })
 }
 
+#ec2role policy attachments
 resource "aws_iam_role_policy_attachment" "attach_s3_policy_to_ec2_role" {
-  role       = aws_iam_role.custom_role_for_ec2_s3_secrets_manager.name
+  role       = aws_iam_role.custom_role_for_ec2_policies.name
   policy_arn = aws_iam_policy.custom_policy_for_s3.arn
 }
 resource "aws_iam_role_policy_attachment" "attach_secrets_manager_policy_to_ec2_role" {
-  role       = aws_iam_role.custom_role_for_ec2_s3_secrets_manager.name
+  role       = aws_iam_role.custom_role_for_ec2_policies.name
   policy_arn = aws_iam_policy.custom_policy_for_secrets_manager.arn
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "${var.project_name}-ec2-profile"
-  role = aws_iam_role.custom_role_for_ec2_s3_secrets_manager.name
+  role = aws_iam_role.custom_role_for_ec2_policies.name
 }
 
+#ecs task execution role policy attachments
 resource "aws_iam_role_policy_attachment" "attach_secret_manager_policy_to_ecs_task_role" {
-  role       = aws_iam_role.custom_role_for_ecs_task_secrets_manager.name
+  role       = aws_iam_role.custom_role_for_ecs_task_policies.name
   policy_arn = aws_iam_policy.custom_policy_for_secrets_manager.arn
 }
-resource "aws_iam_role_policy_attachment" "attach_aws_managed_policies_to_ecs_task_execution_role" {
-  role       = aws_iam_role.custom_role_for_ecs_task_execution_secrets_manager.name
+resource "aws_iam_policy_attachment" "attach_s3_policies_to_ecs_task_role" {
+  role = aws_iam_role.custom_role_for_ecs_task_policies.name
+  policy_arn = aws_iam_policy.custom_policy_for_s3.arn
+}
+resource "aws_iam_policy_attachment" "attach_ssm_full_access_to_ecs_task_role" {
+  role = aws_iam_role.custom_role_for_ecs_task_policies.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+} 
+
+#ecs task execution role policy attachments
+resource "aws_iam_role_policy_attachment" "attach_ecr_to_ecs_task_execution_role" {
+  role       = aws_iam_role.custom_role_for_ecs_task_execution_policies.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 resource "aws_iam_role_policy_attachment" "attach_secret_manager_policy_to_ecs_task_execution_role" {
-  role       = aws_iam_role.custom_role_for_ecs_task_execution_secrets_manager.name
+  role       = aws_iam_role.custom_role_for_ecs_task_execution_policies.name
   policy_arn = aws_iam_policy.custom_policy_for_secrets_manager.arn
+}
+resource "aws_iam_role_policy_attachment" "attach_ssm_full_access_to_ecs_task_execution_role" {
+  role = aws_iam_role.custom_role_for_ecs_task_execution_policies.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
+resource "aws_iam_role_policy_attachment" "attach_cloudwatch_logs_to_ecs_task_execution_role" {
+  role       = aws_iam_role.custom_role_for_ecs_task_execution_policies.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
